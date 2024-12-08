fbdda7e4bda3d304a06e5bb29fc22fa79af35d36
SlowRiot <slowriot@voxelstorm.com>
2024-12-08 14:41:14 +0000

    fix includes and namespace

--- render/uniforms.h
+++ render/uniforms.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "vectorstorm/matrix/matrix3.h"
+#include "vectorstorm/matrix/matrix4.h"
+
+namespace render {
 
 struct alignas(16) uniforms {
   mat4f model_view_projection_matrix;
   mat3fwgpu normal_matrix;
 };
+
+}
